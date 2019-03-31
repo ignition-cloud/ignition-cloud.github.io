fca15d21bfaa3fd23a2bbf504524c56b6f193995
theochupp <tclchiam@gmail.com>
2019-03-31 16:28:00 -0400

    Added build tools for production builds

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpackMerge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+const modeConfig = env => require(`./build-tools/webpack.${env}`)(env);
+const presetConfig = require("./build-tools/loadPresets");
+
+module.exports = ({mode, presets} = {mode: "production", presets: []}) => {
+    console.log(`Building for: ${mode}`);
+
+    return webpackMerge({
+            mode: mode,
+            entry: path.join(__dirname, './src/static/index.js'),
+            module: {
+                rules: [
+                    {
+                        test: /\.(eot|svg|ttf|woff|woff2)$/,
+                        loader: 'file-loader?name=[name].[hash].[ext]'
+                    }
+                ]
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    template: 'src/static/index.html',
+                    inject: 'body',
+                    filename: 'index.html',
+                }),
+
+                new StyleLintPlugin(),
+
+                // new CopyWebpackPlugin([
+                //     { from: 'src/assets/favicon.ico' }
+                // ]),
+            ]
+        },
+        modeConfig(mode),
+        presetConfig({mode: mode, presets: presets})
+    );
+};
