90e87e1cc5d25748319f7a89cb3346ddcacab235
theochupp <tclchiam@gmail.com>
2019-04-01 21:37:54 -0400

    Added value proposition boxes below the "hero"

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,11 +1,13 @@
 module Main exposing (main)
 
 import Browser
+import Bulma.Columns exposing (ColumnModifiers, column, columnModifiers, columns, columnsModifiers)
+import Bulma.Components exposing (navbar, navbarBrand, navbarBurger, navbarItem, navbarModifiers)
 import Bulma.Elements exposing (..)
 import Bulma.Layout exposing (..)
 import Bulma.Modifiers exposing (..)
 import Bulma.Modifiers.Typography exposing (textCentered)
-import Html exposing (Attribute, Html, main_, p, strong, text)
+import Html exposing (Attribute, Html, br, main_, p, span, strong, text)
 
 
 type alias Model =
@@ -38,8 +40,30 @@ update msg model =
 view : Model -> Html Msg
 view _ =
     main_ []
-        [ heroView
+        [ navbarView
+        , heroView
+        , valuePropositionsView
         , footerView
+        ]
+
+
+navbarView : Html Msg
+navbarView =
+    navbar navbarModifiers
+        []
+        [ navbarBrand []
+            (navbarBurger False
+                []
+                [ span [] []
+                , span [] []
+                , span [] []
+                ]
+            )
+            [ navbarItem False
+                []
+                [ title H3 [] [ text "Ignition" ]
+                ]
+            ]
         ]
 
 
@@ -48,12 +72,85 @@ heroView =
     hero { heroModifiers | color = Primary, size = Medium }
         []
         [ heroBody []
-            [ container []
-                [ title H1 [] [ text "Ignition" ]
-                , title H2 [] [ text "Self-service product configuration for complex offerings" ]
+            [ columns columnsModifiers
+                []
+                [ column heroColumnModifiers
+                    []
+                    [ title H1 [] [ text "Simple product modeling for complex offerings" ]
+                    , title H6 [] [ text "Increase your conversion rate by letting customers explore your product themselves" ]
+                    ]
                 ]
             ]
         ]
+
+
+heroColumnModifiers : ColumnModifiers
+heroColumnModifiers =
+    let
+        widths : Devices (Maybe Width)
+        widths =
+            columnModifiers.widths
+    in
+    { columnModifiers
+        | offset = Width1
+        , widths =
+            { widths
+                | tablet = Just Width10
+                , desktop = Just Width5
+                , widescreen = Just Width5
+                , fullHD = Just Width5
+            }
+    }
+
+
+valuePropositionsView : Html Msg
+valuePropositionsView =
+    section NotSpaced
+        []
+        [ columns { columnsModifiers | centered = True, multiline = True }
+            []
+            [ column valuePropColumnModifiers
+                []
+                [ box
+                    []
+                    [ title H6 [] [ text "Guiding customers to find the right product offering for them" ]
+                    ]
+                ]
+            , column valuePropColumnModifiers
+                []
+                [ box
+                    []
+                    [ title H6 [] [ text "Shortening sales cycle by allowing Ignition to handle the high-touch sales" ]
+                    ]
+                ]
+            , column valuePropColumnModifiers
+                []
+                [ box
+                    []
+                    [ title H6 [] [ text "Reducing order errors by providing a unified product model" ]
+                    ]
+                ]
+            ]
+        ]
+
+
+valuePropColumnModifiers : ColumnModifiers
+valuePropColumnModifiers =
+    let
+        widths : Devices (Maybe Width)
+        widths =
+            columnModifiers.widths
+    in
+    { columnModifiers
+        | offset = Auto
+        , widths =
+            { widths
+                | tablet = Just Width11
+                , desktop = Just Width6
+                , widescreen = Just Width4
+                , fullHD = Just Width3
+            }
+    }
 
 
 footerView : Html Msg
@@ -63,8 +160,7 @@ footerView =
             [ content Standard
                 [ textCentered ]
                 [ p []
-                    [ text "©"
-                    , text " 2019 "
+                    [ text "© 2019 "
                     , strong [] [ text "Ignition" ]
                     ]
                 ]
